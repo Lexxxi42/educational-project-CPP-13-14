cf481df178bbd9162df5b1619a7598f58484f142
joanegla <joanegla@student.21-school.ru>
2026-05-21 22:13:55 +0500

    add: first virtual func := get_info()

--- main.cpp
+++ main.cpp
@@ -5,65 +5,101 @@ using namespace std;
 // ! ||--------------------------------------------------------------------------------||
 // ! ||                           основной родительский класс                          ||
 // ! ||--------------------------------------------------------------------------------||
+
 class Microorganisms {
     string name;
-    int size;
-    static int min_size;
+    long int size;
+    static long int min_size;
 public:
-    Microorganisms(string name = "Unknown", int size = 0) {
+    Microorganisms(string name = "Unknown", long int size = 0) {
         if (size < min_size)
             min_size = size;
         this->name = name;
         this->size = size;
     }
+
     int get_size() {return size;}
     static int get_min_size() {return min_size;}
     string get_name() {return name;}
+
+    virtual void get_info() {
+        cout << "Name: " << name << endl;
+        cout << "Size: " << size << endl;
+    }
 };
 
-int Microorganisms::min_size = 5;
+long int Microorganisms::min_size = 5;
+
 // ! ||--------------------------------------------------------------------------------||
 // ! ||                                    подклассы                                   ||
 // ! ||--------------------------------------------------------------------------------||
+
 class Viruses:public Microorganisms {
     int is_dangerous;
 public:
-    Viruses(string name, int size, int is_dangerous):Microorganisms(name,size){
-        this->is_dangerous = is_dangerous;
+    Viruses(string name, long int size, int is_dangerous = 1):Microorganisms(name,size){
+        if (is_dangerous < 0 && is_dangerous > 1) this->is_dangerous = 1;
+        else this->is_dangerous = is_dangerous;
+    }
+
+    void get_info() {
+        Microorganisms::get_info();
+        cout << "Is dangerous: " << is_dangerous << endl;
     }
 };  
 
 class Prokaryotes:public Microorganisms {
     int metabolism;
 public:
-    Prokaryotes(string name, int size, int metabolism):Microorganisms(name,size){
+    Prokaryotes(string name, long int size, int metabolism):Microorganisms(name,size){
         this->metabolism = metabolism;
+    }
+
+    void get_info() {
+        Microorganisms::get_info();
+        cout << "Metabolism: " << metabolism << endl;
     }
 };
 
 class Eukaryotes:public Microorganisms {
     int reproduction;
 public:
-    Eukaryotes(string name, int size, int reproduction):Microorganisms(name,size){
+    Eukaryotes(string name, long int size, int reproduction):Microorganisms(name,size){
         this->reproduction = reproduction;
     }
+
+    void get_info() {
+        Microorganisms::get_info();
+        cout << "Reproduction: " << reproduction << endl;
+    }
 };
+
 // ! ||--------------------------------------------------------------------------------||
 // ! ||                                  подподклассы                                  ||
 // ! ||--------------------------------------------------------------------------------||
 class Bacteria:public Prokaryotes {
     int gram_stain;
 public:
-    Bacteria(string name, int size, int metabolism, int gram_stain):Prokaryotes(name,size,metabolism){
+    Bacteria(string name, long int size, int metabolism, int gram_stain):Prokaryotes(name,size,metabolism){
         this->gram_stain = gram_stain;
+    }
+
+    void get_info() {
+        Prokaryotes::get_info();
+        cout << "Gram stain: " << gram_stain << endl;
     }
 };
 
 class Archaea:public Prokaryotes {
     int membrane_structure;
 public:
-    Archaea(string name, int size, int metabolism, int membrane_structure):Prokaryotes(name,size,metabolism){
+    Archaea(string name, long int size, int metabolism, int membrane_structure):Prokaryotes(name,size,metabolism){
         this->membrane_structure = membrane_structure;
+    }
+
+    void get_info() {
+        Prokaryotes::get_info();
+        cout << "Membrane structure: " << membrane_structure << endl;
     }
 };
 
@@ -71,24 +107,39 @@ public:
 class Animals: public Eukaryotes {
     int is_multicellular;
 public:
-    Animals(string name, int size, int reproduction, int is_multicellular):Eukaryotes(name, size, reproduction) {
+    Animals(string name, long int size, int reproduction, int is_multicellular):Eukaryotes(name, size, reproduction) {
             this->is_multicellular = is_multicellular;
+    }
+
+    void get_info() {
+        Eukaryotes::get_info();
+        cout << "Is multicellular: " << is_multicellular << endl;
     }
 };
 
 class Plants: public Eukaryotes {
     int is_higher_realms;
 public:
-    Plants(string name, int size, int reproduction, int is_higher_realms):Eukaryotes(name, size, reproduction) {
+    Plants(string name, long int size, int reproduction, int is_higher_realms):Eukaryotes(name, size, reproduction) {
             this->is_higher_realms = is_higher_realms;
+    }
+
+    void get_info() {
+        Eukaryotes::get_info();
+        cout << "Is higher realms: " << is_higher_realms << endl;
     }
 };
 
 class Mushrooms: public Eukaryotes {
     int is_multicellular;
 public:
-    Mushrooms(string name, int size, int reproduction, int is_multicellular):Eukaryotes(name, size, reproduction) {
+    Mushrooms(string name, long int size, int reproduction, int is_multicellular):Eukaryotes(name, size, reproduction) {
             this->is_multicellular = is_multicellular;
+    }
+
+    void get_info() {
+        Eukaryotes::get_info();
+        cout << "Is multicellular: " << is_multicellular << endl;
     }
 };
 
@@ -144,10 +195,18 @@ int main(){
     list--;
     list.print();
 
-    Viruses* virus = new Viruses("Virus", 100, 1);
+    Viruses* virus = new Viruses("Virus", 100);
     Viruses* virus2 = new Viruses("Jolin", 1, 1);
     cout << virus->get_name() << endl;
     cout << virus->get_size() << endl;
     cout << virus2->get_min_size() << endl;
+    cout << endl;
+    virus->get_info();
+    cout << endl;
+    virus2->get_info();
+    cout << endl;
+    Animals* lion = new Animals("Lion", 250000000000, 1, 1);
+    lion->get_info();
+    
     return 0;
 }
